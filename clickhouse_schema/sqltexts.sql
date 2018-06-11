daa37697570caea03b3ee02dcbd05de8238adc08
Tribein <tribein@live.ru>
2018-06-11 14:39:43 +0300

    Delete sqltexts.sql

--- clickhouse_schema/sqltexts.sql
+++
@@ -1,2 +0,0 @@
-CREATE TABLE oradb.sqltexts ( snapDate Date,  sqlid FixedString(13),  sqltext String,  snapTime DateTime) ENGINE = ReplacingMergeTree(snapDate, sqlid, 8192)
-CREATE TABLE oradb.sqltexts_buffer ( snapDate Date,  sqlid FixedString(13),  sqltext String,  snapTime DateTime) ENGINE = Buffer('oradb', 'sqltexts', 16, 900, 14400, 1000000, 10000000, 10000000, 1000000000)
