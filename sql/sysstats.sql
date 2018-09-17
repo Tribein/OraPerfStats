ee6adc8d586d4e5412fe4a4f4f3ff4070347067b
Tribein <tribein@live.ru>
2018-09-17 12:22:58 +0300

    Update sysstats.sql

--- sql/sysstats.sql
+++ sql/sysstats.sql
@@ -1,2 +1,2 @@
-CREATE TABLE oradb.sysstats ( dbuniquename String, snapDate Date, snapTime DateTime, statId UInt16, value Int64 ) ENGINE = MergeTree(snapDate, dbuniquename, 1024);
+CREATE TABLE oradb.sysstats ( dbuniquename String, snapDate Date, snapTime DateTime, statId UInt16, value Int64 ) ENGINE = MergeTree() partition by (snapDate) order by (dbuniquename,snapTime) SETTINGS index_granularity=8192;
 CREATE TABLE oradb.sysstats_buffer AS oradb.sysstats ENGINE = Buffer(oradb, sysstats, 16, 14400, 86400, 100000, 1000000, 10000000, 100000000);
